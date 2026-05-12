0f794e322d85a5cb95b0ffd791d63803be035342
Nemoeiei <108443709+Nemoeiei@users.noreply.github.com>
2026-05-13 00:12:03 +0700

    fix: global HttpOverrides in debug mode - bypass SSL for NetworkImage and all HTTPS on emulator

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
+import 'dart:io';
 
-import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/foundation.dart' show kDebugMode, kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -21,7 +22,18 @@ import 'theme/app_theme.dart';
 import 'constants/constants.dart';
 import 'widget/bottom_bar.dart';
 
+class _DevHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (cert, host, port) => true;
+  }
+}
+
 void main() async {
+  if (!kIsWeb && kDebugMode) {
+    HttpOverrides.global = _DevHttpOverrides();
+  }
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase (ต้องมาก่อน runApp และก่อน Supabase)
